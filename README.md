2ac419daa8147bb92929b2d8c52c548a8ed90664
Jarrod Bataille <jtbataille@gmail.com>
2020-08-24 16:25:32 -0400

    test3

---
+++ README.md
@@ -0,0 +1,34 @@
+
+# README.md Generator
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
+## Description
+woeij
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributors](#contributors)
+* [Tests](#tests)
+* [Contact Information](#contact-information)
+
+## Installation
+As a user, you will need to instal the following packages: inquirer
+
+## Usage
+Here is how you can use this program: woeifjwe
+
+## License
+Copyright © 2007 Free Software Foundation, Inc. <https://fsf.org/>
+
+## Contributors
+Jarrod Bataille
+
+## Tests
+In order to test this program, use the following command: node index.js
+
+## Contact Information
+* Jarrod Bataille
+  * GitHub Profile: https://github.com/jtbataille
+  * Please email me with any questions here: jtbataille@me.com
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,13 +3,10 @@ const license = value => {
   switch (value) {
     case "MIT":
       return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
-
     case "GNU":
       return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
-
     case "ISC":
       return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
-
     default:
       return "";
   }
@@ -18,14 +15,11 @@ const license = value => {
 const licenseFieldText = (value, name) => {
   switch (value) {
     case "MIT":
-      return 'Copyright © 2020 ' + name;
-      
+      return 'Copyright © 2020 ' + name; 
     case "GNU":
       return 'Copyright © 2007 Free Software Foundation, Inc. <https://fsf.org/>';
-
     case "ISC":
       return 'Copyright 2020 ' + name;
-
     default :
       return "";
   }
@@ -33,40 +27,42 @@ const licenseFieldText = (value, name) => {
 
 module.exports = {
   generateMarkdown: data => {
-    return `
-    # ${data.title}
-    ${license(data.license)}
-  
-    ## Description
-    ${data.description}
-  
-    ## Table of Contents
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [License](#license)
-    * [Contributors](#contributors)
-    * [Tests](#tests)
-    * [Contact Information](#contact-information)
     
-    ## Installation
-    As a user, you will need to instal the following packages: ${data.installation}
-  
-    ## Usage
-    Here is how you can use this program: ${data.usage}
-  
-    ## License
-    ${licenseFieldText(data.license, data.name)}
-  
-    ## Contributors
-    ${data.contributors}
-  
-    ## Tests
-    In order to test this program, use the following command: ${data.tests}
-  
-    ## Contact Information
-    * ${data.name}
-      * GitHub Profile: https://github.com/${data.username}
-      * Please email me with any questions here: ${data.email}
-  `;
+    
+    return `
+# ${data.title}
+${license(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributors](#contributors)
+* [Tests](#tests)
+* [Contact Information](#contact-information)
+
+## Installation
+As a user, you will need to instal the following packages: ${data.installation}
+
+## Usage
+Here is how you can use this program: ${data.usage}
+
+## License
+${licenseFieldText(data.license, data.name)}
+
+## Contributors
+${data.contributors}
+
+## Tests
+In order to test this program, use the following command: ${data.tests}
+
+## Contact Information
+* ${data.name}
+  * GitHub Profile: https://github.com/${data.username}
+  * Please email me with any questions here: ${data.email}
+`;
   }
 };
